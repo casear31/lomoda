3ad778965ac6282de30aecdd55467bd52e191560
casear31 <caesar31c@gmail.com>
2021-07-08 18:56:11 +0300

    output  from db

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@
 
 const headerCityButton = document.querySelector('.header__city-button');
 
+//hash (category)
+let hash = location.hash.substring(1);
+
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
 headerCityButton.addEventListener('click', () => {
@@ -43,12 +46,7 @@ const enableScroll = () => {
 
 // modal window
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
-        closeModal();
-        enableScroll();
-    }
-})
+
 
 const subheaderCart = document.querySelector('.subheader__cart'),
     cartOverlay = document.querySelector('.cart-overlay');
@@ -62,6 +60,45 @@ const openModal = () => {
     disableScroll();
 }
 
+
+
+// query to DB
+
+const getData = async () => {
+    const data = await fetch('db.json');
+
+    if (data.ok) {
+        return data.json();
+    } else {
+        throw new Error(`Данные не были получены, ошибка: ${data.status} ${data.statusText}`)
+    }
+}
+
+const getGoods = (callback, value) => {
+    getData()
+        .then(data => {
+            if (value) {
+                callback(data.filter(item => item.category === value))
+            } else {
+                callback(data);
+            }
+            
+        })
+        .catch(err => {
+            console.error(err);
+        });
+};
+
+
+    
+//move here from modal window
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        closeModal();
+        enableScroll();
+    }
+})
+
 subheaderCart.addEventListener('click', openModal);
 
 cartOverlay.addEventListener('click', event => {
@@ -73,3 +110,74 @@ cartOverlay.addEventListener('click', event => {
 
 
 
+// output goods on the page
+
+try {
+    const goodsList = document.querySelector('.goods__list');
+
+    if (!goodsList) {
+        throw 'This is not a goods page'
+    }
+
+    const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+
+// the same as   data -> callback
+        // const id = data.id,
+        //     preview = data.preview,
+        //     cost = data.cost,
+        //     brand = data.brand,
+        //     name = data.name,
+        //     sizes = data.sizes;
+
+
+        const li = document.createElement('li');
+
+        li.classList.add('goods__item');
+
+        li.innerHTML = `
+            <article class="good">
+                            <a class="good__link-img" href="card-good.html#${id}">
+                                <img class="good__img" src="goods-image/${preview}" alt="">
+                            </a>
+                            <div class="good__description">
+                                <p class="good__price">${cost} &#8381;</p>
+                                <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                                ${sizes ? `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` : ''}
+                                <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+                            </div>
+                        </article>
+        `;
+
+        return li;
+    };
+    const renderGoodsList = data => {
+        goodsList.textContent = '';
+        
+        data.forEach(item => {
+            const card = createCard(item);
+
+            goodsList.append(card);
+        });
+
+    };
+    
+    
+    
+    window.addEventListener('hashchange', () => {
+        
+        const categoryTitle = document.querySelector('.goods__title');
+
+        hash = location.hash.substring(1);
+
+        categoryTitle.textContent = document.querySelector(`a[href*="#${hash}"]`).textContent;
+        getGoods(renderGoodsList, hash);
+    })
+
+    getGoods(renderGoodsList);
+
+
+    
+} catch(err) {
+    console.warn(err)
+    
+}
